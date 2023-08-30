7ffca7a4e7e2f73182d90fffcd519340239dbf5b
horusboyd <thafunkyequine@hotmail.com>
2023-08-30 15:11:21 +0200

    Update 1984bot.py

--- 1984bot.py
+++ 1984bot.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents(messages=True, message_content=True)
 bot = commands.Bot(command_prefix='!', intents=intents)
